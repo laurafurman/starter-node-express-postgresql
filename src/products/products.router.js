bf11d2f6c1114afada4eee82d0236c607f708e7a
Laura Furman <98051403+laurafurman@users.noreply.github.com>
2022-06-24 18:44:05 -0500

    Midway through lesson

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -1,8 +1,8 @@
-const router = require("express").Router({ mergeParams: true });
+const router = require("express").Router();
 const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId").get(controller.read).all(methodNotAllowed);
+router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
 module.exports = router;
